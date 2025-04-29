3bcb964188773aafe563459a41e2ce1fa3217170
신해인 <godls0215@naver.com>
2025-04-29 23:27:40 +0900

    fix: ESLint 및 Prettier 설정 파일 업데이트 및 패키지 버전 조정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,43 @@
 module.exports = {
-root: true,
-extends: [
-'@react-native',
-'eslint:recommended',
-'plugin:react/recommended',
-'plugin:react-hooks/recommended',
-'plugin:prettier/recommended',
-],
-parser: '@babel/eslint-parser',
-parserOptions: {
-ecmaFeatures: {
-    jsx: true,
-},
-ecmaVersion: 2020,
-sourceType: 'module',
-},
-plugins: ['react', 'react-hooks', 'prettier'],
-rules: {
-'prettier/prettier': 'error',
-'react/react-in-jsx-scope': 'off',
-'react/prop-types': 'off',
-'no-console': ['warn', { allow: ['warn', 'error'] }],
-'react-hooks/rules-of-hooks': 'error',
-'react-hooks/exhaustive-deps': 'warn',
-},
-settings: {
-react: {
-    version: 'detect',
-},
-},
-env: {
-'react-native/react-native': true,
-es6: true,
-node: true,
-},
-}; 
+  root: true,
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-native', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/no-inline-styles': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+  },
+};
