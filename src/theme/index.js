c112ccb7e8f8dee16ff85784d0a6c1cc6e3263c4
신해인 <godls0215@naver.com>
2025-04-29 23:48:05 +0900

    design: 테마 설정 index.js에 색상, 레이아웃, 타이포그래피, border 추가

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+import colors from './colors';
+import layout from './layout';
+import typography from './typography';
+import border from './border';
+
+export default {
+  colors,
+  layout,
+  typography,
+  border,
+};
