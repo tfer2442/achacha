7c94d36dc728476ae479970f32be1f4608be68ed
신해인 <godls0215@naver.com>
2025-04-30 00:41:14 +0900

    feat: 메인 네비게이터 추가 및 BottomTabBar 컴포넌트 연결

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SplashScreenComponent from '../screens/SplashScreen';
 import GuideFirstScreen from '../screens/GuideFirstScreen';
+import BottomTabBar from '../components/common/BottomTabBar';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,14 +14,21 @@ const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+      {/* 로그인 및 온보딩 화면 (탭바 없음) */}
       <Stack.Screen name="Splash" component={SplashScreenComponent} />
       <Stack.Screen name="GuideFirst" component={GuideFirstScreen} />
       <Stack.Screen name="Permission" component={PermissionScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      {/* 다른 스크린들을 여기에 추가할 수 있습니다 */}
+
+      {/* 메인 탭바 화면 */}
+      <Stack.Screen name="Main" component={MainNavigator} />
     </Stack.Navigator>
   );
+};
+
+// 메인 네비게이터 (탭바가 있는 화면들)
+const MainNavigator = () => {
+  return <BottomTabBar />;
 };
 
 export default AppNavigator;
