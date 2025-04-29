87f4b59ddc77e7f1331a841f66d55aa3ab5d6ce0
신해인 <godls0215@naver.com>
2025-04-29 23:54:00 +0900

    design: border.js 파일의 테두리 반경 및 두께 값 업데이트, 스타일 추가

--- src/theme/border.js
+++ src/theme/border.js
@@ -1,13 +1,25 @@
+// React Native에서는 단위 없이 숫자로만 표현 (px가 아님)
 const border = {
   radius: {
-    sm: '5px',
-    md: '10px',
-    lg: '15px',
+    none: 0,
+    extraSmall: 2,
+    small: 4,
+    medium: 8,
+    large: 12,
+    extraLarge: 16,
+    full: 9999,
   },
   width: {
-    thin: '1px',
-    medium: '2px',
-    thick: '3px',
+    none: 0,
+    thin: 1,
+    medium: 2,
+    thick: 3,
+    extraThick: 4,
+  },
+  style: {
+    solid: 'solid',
+    dashed: 'dashed',
+    dotted: 'dotted',
   },
 };
 
