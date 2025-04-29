118d5669b173230eda977f4a84d3065728693e30
신해인 <godls0215@naver.com>
2025-04-29 23:37:23 +0900

    design: border 테마 값 설정

---
+++ src/theme/border.js
@@ -0,0 +1,14 @@
+const border = {
+  radius: {
+    sm: '5px',
+    md: '10px',
+    lg: '15px',
+  },
+  width: {
+    thin: '1px',
+    medium: '2px',
+    thick: '3px',
+  },
+};
+
+export default border;
