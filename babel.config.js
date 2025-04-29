21c5912b7901e2dd6b2189ca6f1e1564bb0dd3e5
신해인 <godls0215@naver.com>
2025-04-29 21:01:29 +0900

    feat: react-native-reanimated 플러그인 추가 및 관련 패키지 설치

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: [
+      // 애니메이션을 위한 Reanimated 플러그인
+      'react-native-reanimated/plugin',
+    ],
   };
 }; 
