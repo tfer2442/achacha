fde425ad9d2c8d02e256d839563d5444e16974c4
류재문 <woans5970@naver.com>
2025-04-30 20:37:07 +0900

    chore: 프로젝트 생성

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/AchachaApplication.java
@@ -0,0 +1,13 @@
+package com.eurachacha.achacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchachaApplication.class, args);
+	}
+
+}
---
+++ backend/achacha/src/test/java/com/eurachacha/achacha/AchachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurachacha.achacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
