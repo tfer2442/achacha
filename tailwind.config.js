adcc501a10836851e510db110e74db9c9cce394a
신해인 <godls0215@naver.com>
2025-04-30 15:05:36 +0900

    feat: gluestack ui 사용을 위한 설치 및 설정 파일 수정

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          new: 'var(--color-background-new)',
+        },
+        primary: {
+          0: 'var(--color-primary-0)',
+          50: 'var(--color-primary-50)',
+          100: 'var(--color-primary-100)',
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          500: 'var(--color-primary-500)',
+          600: 'var(--color-primary-600)',
+          700: 'var(--color-primary-700)',
+          800: 'var(--color-primary-800)',
+          900: 'var(--color-primary-900)',
+        },
+      },
+      fontSize: {
+        'custom-heading-xl': 'var(--custom-font-size)',
+      },
+    },
+  },
+  plugins: [],
+};
