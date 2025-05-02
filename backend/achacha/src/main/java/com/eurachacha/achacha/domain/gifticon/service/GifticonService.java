999f07834dbd62c09cbc8a59d57b18909c73dc54
류재문 <woans5970@naver.com>
2025-05-02 16:01:23 +0900

    feat: 임시 기프티콘 등록 서비스 로직 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/gifticon/service/GifticonService.java
@@ -0,0 +1,7 @@
+package com.eurachacha.achacha.domain.gifticon.service;
+
+import com.eurachacha.achacha.domain.gifticon.entity.Gifticon;
+
+public interface GifticonService {
+	Gifticon saveGifticon(Gifticon gifticon); // 임시용
+}
