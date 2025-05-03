5a99707195741d68facf432396134c5dd226cb27
신해인 <godls0215@naver.com>
2025-05-04 05:12:49 +0900

    feat: Divider 컴포넌트 추가

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -12,3 +12,4 @@ export { default as Card } from './Card';
 export { default as Chip } from './Chip';
 export { default as ListItem } from './ListItem';
 export { default as CheckBox } from './CheckBox';
+export { default as Divider } from './Divider';
