8e4410c861818c5e778770d73b2fbd2b14a2e402
류재문 <woans5970@naver.com>
2025-05-05 00:47:46 +0900

    feat: 도메인 서비스 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainService.java
@@ -0,0 +1,4 @@
+package com.eurachacha.achacha.domain.service.gifticon;
+
+public interface AvailableGifticonDomainService {
+}
---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainServiceImpl.java
@@ -0,0 +1,7 @@
+package com.eurachacha.achacha.domain.service.gifticon;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AvailableGifticonDomainServiceImpl implements AvailableGifticonDomainService {
+}
