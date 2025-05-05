09f98b3b0ade31b040739c76f5b8618e7fb0a329
류재문 <woans5970@naver.com>
2025-05-05 21:22:20 +0900

    style: check-style 적용

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/gifticon/AvailableGifticonRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/gifticon/AvailableGifticonRepository.java
@@ -1,13 +1,16 @@
 package com.eurachacha.achacha.application.port.output.gifticon;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonDetailResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonResponseDto;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonScopeType;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 public interface AvailableGifticonRepository {
-    Slice<AvailableGifticonResponseDto> getAvailableGifticons(Integer userId, GifticonScopeType scope, GifticonType type, Pageable pageable);
-    AvailableGifticonDetailResponseDto getAvailableGifticonDetail(Integer userId, Integer gifticonId);
+	Slice<AvailableGifticonResponseDto> getAvailableGifticons(Integer userId, GifticonScopeType scope,
+		GifticonType type, Pageable pageable);
+
+	AvailableGifticonDetailResponseDto getAvailableGifticonDetail(Integer userId, Integer gifticonId);
 }
