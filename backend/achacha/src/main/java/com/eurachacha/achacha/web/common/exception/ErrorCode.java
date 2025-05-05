a3e2237ba845a4a03939e30e9df466a835aa712e
류재문 <woans5970@naver.com>
2025-05-05 19:14:17 +0900

    feat: 기프티콘 관련 에러코드 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/common/exception/ErrorCode.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/common/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     EXAMPLE_NOT_FOUND(HttpStatus.NOT_FOUND, "X001", "예제 에러 코드입니다"),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "X002", "잘못된 파라미터가 전달되었습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "X003", "서버 에러가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "X003", "서버 에러가 발생했습니다."),
+
+    // 기프티콘 관련 에러 코드
+    GIFTICON_NOT_FOUND(HttpStatus.NOT_FOUND, "GIFTICON_001", "기프티콘 정보를 찾을 수 없습니다,"),
+    UNAUTHORIZED_GIFTICON_ACCESS(HttpStatus.FORBIDDEN, "GIFTICON_002", "해당 기프티콘에 접근 권한이 없습니다.");
 
     // http 상태 코드
     private final HttpStatus status;
