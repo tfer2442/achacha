a3f5e76db83e2cfa15ab30ba9560bdcd6db5a3de
류재문 <woans5970@naver.com>
2025-05-05 19:14:01 +0900

    feat: application <-> infrastructure 연결 어댑터 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/gifticon/AvailableGifticonPersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/gifticon/AvailableGifticonPersistenceAdapter.java
@@ -1,10 +1,13 @@
 package com.eurachacha.achacha.infrastructure.adapter.output.persistence.gifticon;
 
+import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonDetailResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonResponseDto;
 import com.eurachacha.achacha.application.port.output.gifticon.AvailableGifticonRepository;
 import com.eurachacha.achacha.domain.model.gifticon.enums.FileType;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonScopeType;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
+import com.eurachacha.achacha.web.common.exception.CustomException;
+import com.eurachacha.achacha.web.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -19,5 +22,17 @@ public class AvailableGifticonPersistenceAdapter implements AvailableGifticonRep
     @Override
     public Slice<AvailableGifticonResponseDto> getAvailableGifticons(Integer userId, GifticonScopeType scope, GifticonType type, Pageable pageable) {
         return availableGifticonJpaRepository.findAvailableGifticons(userId, scope, type, FileType.THUMBNAIL, pageable);
+    }
+
+    @Override
+    public AvailableGifticonDetailResponseDto getAvailableGifticonDetail(Integer userId, Integer gifticonId) {
+
+        AvailableGifticonDetailResponseDto availableGifticonDetail = availableGifticonJpaRepository.findAvailableGifticonDetail(gifticonId, userId);
+
+        if (availableGifticonDetail == null) {
+            throw new CustomException(ErrorCode.GIFTICON_NOT_FOUND);
+        }
+
+        return availableGifticonDetail;
     }
 }
