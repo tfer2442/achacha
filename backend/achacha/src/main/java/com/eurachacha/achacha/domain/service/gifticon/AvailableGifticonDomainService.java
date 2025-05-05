c4e57045f3462b7141f6f6a790d17dcd4895b2d6
류재문 <woans5970@naver.com>
2025-05-05 19:14:32 +0900

    feat: 소유자 검증 로직 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainService.java
@@ -1,4 +1,5 @@
 package com.eurachacha.achacha.domain.service.gifticon;
 
 public interface AvailableGifticonDomainService {
+    void validateGifticonAccess(Integer requestUserId, Integer gifticonUserId);
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainServiceImpl.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/AvailableGifticonDomainServiceImpl.java
@@ -1,7 +1,18 @@
 package com.eurachacha.achacha.domain.service.gifticon;
 
+import com.eurachacha.achacha.web.common.exception.CustomException;
+import com.eurachacha.achacha.web.common.exception.ErrorCode;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AvailableGifticonDomainServiceImpl implements AvailableGifticonDomainService {
+
+    @Override
+    public void validateGifticonAccess(Integer requestUserId, Integer gifticonUserId) {
+        if (!Objects.equals(requestUserId, gifticonUserId)) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_GIFTICON_ACCESS);
+        }
+    }
 }
