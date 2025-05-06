85e7d7ae0af9a091e087f7c8c471b8dbc65cd7a7
류재문 <woans5970@naver.com>
2025-05-06 20:26:43 +0900

    feat: 사용 타입 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/history/enums/UsageType.java
@@ -0,0 +1,11 @@
+package com.eurachacha.achacha.domain.model.history.enums;
+
+/**
+ * 사용 유형 열거형
+ * 사용 유형을 정의
+ */
+public enum UsageType {
+	SELF_USE, // 본인 사용
+	GIVE_AWAY, // 나눔하기(뿌리기)
+	PRESENT, // 링크로 선물(외부 공유)
+}
