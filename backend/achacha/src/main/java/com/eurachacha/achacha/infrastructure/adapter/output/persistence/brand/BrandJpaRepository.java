354eb60916219ce1c80d35cc3af6e54454aee8ab
조대성 <tfer2442@naver.com>
2025-05-06 22:19:35 +0900

    feat: 브랜드 검색 레포지토리 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/brand/BrandJpaRepository.java
@@ -0,0 +1,13 @@
+package com.eurachacha.achacha.infrastructure.adapter.output.persistence.brand;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eurachacha.achacha.domain.model.brand.Brand;
+
+public interface BrandJpaRepository extends JpaRepository<Brand, Integer> {
+
+	// LIKE 연산자를 사용한 검색 + 이름순 정렬
+	List<Brand> findByNameContainingIgnoreCaseOrderByNameAsc(String keyword);
+}
