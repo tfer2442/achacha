517ef69785d32ae319b9eab9f31bfca7786a29a3
ssuzyn <asj7090@gmail.com>
2025-05-07 05:29:30 +0900

    feat: 파일 영속성 어댑터 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/file/FilePersistenceAdapter.java
@@ -0,0 +1,43 @@
+package com.eurachacha.achacha.infrastructure.adapter.output.persistence.file;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.eurachacha.achacha.application.port.output.file.FileRepository;
+import com.eurachacha.achacha.domain.model.gifticon.File;
+import com.eurachacha.achacha.domain.model.gifticon.enums.FileType;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class FilePersistenceAdapter implements FileRepository {
+
+	private final FileJpaRepository fileJpaRepository;
+
+	@Override
+	public File save(File file) {
+		return fileJpaRepository.save(file);
+	}
+
+	@Override
+	public Optional<File> findByReferenceEntityTypeAndReferenceEntityIdAndType(
+		String referenceEntityType, Integer referenceEntityId, FileType type) {
+		return fileJpaRepository.findByReferenceEntityTypeAndReferenceEntityIdAndType(
+			referenceEntityType, referenceEntityId, type);
+	}
+
+	@Override
+	public List<File> findAllByReferenceEntityTypeAndReferenceEntityId(
+		String referenceEntityType, Integer referenceEntityId) {
+		return fileJpaRepository.findAllByReferenceEntityTypeAndReferenceEntityId(
+			referenceEntityType, referenceEntityId);
+	}
+
+	@Override
+	public void delete(File file) {
+		fileJpaRepository.delete(file);
+	}
+}
