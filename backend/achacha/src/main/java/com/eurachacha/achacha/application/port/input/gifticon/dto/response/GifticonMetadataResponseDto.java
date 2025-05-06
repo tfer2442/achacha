eda5b36e78a078b4f8cf796259513ba46dd430e9
조대성 <tfer2442@naver.com>
2025-05-06 17:33:46 +0900

    feat: AI 서비스 포트 인터페이스 및 DTO 정의

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/response/GifticonMetadataResponseDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/response/GifticonMetadataResponseDto.java
@@ -5,14 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@ToString
 public class GifticonMetadataResponseDto {
-	private String gifticonBarcodeNumber; // 바코드 번호
+	private String gifticonBarcodeNumber; // 바코드 번호 (공백과 구분자 없이)
 	private String brandName; // 브랜드 이름
 	private String gifticonName; // 상품명
 	private String gifticonExpiryDate; // 유효기간(YYYY-MM-DD 형식)
+	private Integer gifticonOriginalAmount; // 기프티콘 금액 (금액형인 경우)
 }
