71ae086105a077e04927d671b5ce7c4e92a22c9b
asj7090 <asj7090@gmail.com>
2025-05-07 14:17:20 +0900

    comment: S3, CloudFront 이미지 처리 테스트용 클래스 주석 처리

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/file/FileAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/file/FileAppService.java
@@ -1,18 +1,18 @@
-package com.eurachacha.achacha.application.port.input.file;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.eurachacha.achacha.application.port.input.file.dto.response.FileResponseDto;
-import com.eurachacha.achacha.domain.model.gifticon.enums.FileType;
-
-public interface FileAppService {
-
-	void saveGifticonFiles(Integer gifticonId,
-		MultipartFile originalImage,
-		MultipartFile thumbnailImage,
-		MultipartFile barcodeImage);
-
-	FileResponseDto getGifticonFileUrl(Integer gifticonId, FileType fileType);
-
-	void deleteGifticonFiles(Integer gifticonId);
-}
+// package com.eurachacha.achacha.application.port.input.file;
+//
+// import org.springframework.web.multipart.MultipartFile;
+//
+// import com.eurachacha.achacha.application.port.input.file.dto.response.FileResponseDto;
+// import com.eurachacha.achacha.domain.model.gifticon.enums.FileType;
+//
+// public interface FileAppService {
+//
+// 	void saveGifticonFiles(Integer gifticonId,
+// 		MultipartFile originalImage,
+// 		MultipartFile thumbnailImage,
+// 		MultipartFile barcodeImage);
+//
+// 	FileResponseDto getGifticonFileUrl(Integer gifticonId, FileType fileType);
+//
+// 	void deleteGifticonFiles(Integer gifticonId);
+// }
