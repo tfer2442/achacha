812f79ed1e09fceddb22394eb230bed6e32b723a
조대성 <tfer2442@naver.com>
2025-05-08 06:10:14 +0900

    feat: 기프티콘 DTO 클래스 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonSaveRequestDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonSaveRequestDto.java
@@ -1,18 +1,39 @@
 package com.eurachacha.achacha.application.port.input.gifticon.dto.request;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-public class GifticonSaveRequestDto {
-	private String name;
-	private GifticonType type;
-	private String barcode;
-	private Integer originalAmount;
-	private LocalDate expiryDate;
+@NoArgsConstructor
+public class GifticonSaveRequestDto implements Serializable {
+	@NotBlank(message = "바코드 번호는 필수입니다")
+	private String gifticonBarcodeNumber;
+
+	@NotNull(message = "브랜드 ID는 필수입니다")
+	private Integer brandId;
+
+	@NotBlank(message = "상품명은 필수입니다")
+	private String gifticonName;
+
+	@NotNull(message = "유효기간은 필수입니다")
+	private LocalDate gifticonExpiryDate;
+
+	@NotNull(message = "기프티콘 타입은 필수입니다")
+	private GifticonType gifticonType;
+
+	private Integer gifticonAmount;
+
+	private Integer shareBoxId;
+
+	@NotBlank(message = "OCR 학습 데이터 ID는 필수입니다")
+	private String ocrTrainingDataId;
 }
