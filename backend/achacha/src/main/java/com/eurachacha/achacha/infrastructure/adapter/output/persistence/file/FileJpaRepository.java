9e4cadfb987ad25451adccded66ba71b7c74f6d7
asj7090 <asj7090@gmail.com>
2025-05-07 15:52:58 +0900

    fix: FileJpaRepository 어노테이션 누락 수정

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/file/FileJpaRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/file/FileJpaRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.eurachacha.achacha.domain.model.gifticon.File;
 import com.eurachacha.achacha.domain.model.gifticon.enums.FileType;
 
+@Repository
 public interface FileJpaRepository extends JpaRepository<File, Long> {
 
 	Optional<File> findByReferenceEntityTypeAndReferenceEntityIdAndType(
