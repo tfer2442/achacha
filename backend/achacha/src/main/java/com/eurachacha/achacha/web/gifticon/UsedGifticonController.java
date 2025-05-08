c610bf306407a21be4f60be7b76daa1b8de656b0
류재문 <woans5970@naver.com>
2025-05-08 16:03:37 +0900

    feat: 사용완료 기프티콘 상세 조회 api 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/gifticon/UsedGifticonController.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/gifticon/UsedGifticonController.java
@@ -2,11 +2,13 @@ package com.eurachacha.achacha.web.gifticon;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eurachacha.achacha.application.port.input.gifticon.GifticonAppService;
+import com.eurachacha.achacha.application.port.input.gifticon.dto.response.UsedGifticonDetailResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.UsedGifticonsResponseDto;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonUsedSortType;
@@ -28,5 +30,10 @@ public class UsedGifticonController {
 		@RequestParam(required = false, defaultValue = "0") @Min(0) Integer page,
 		@RequestParam(required = false, defaultValue = "10") @Min(1) Integer size) {
 		return ResponseEntity.ok(gifticonAppService.getUsedGifticons(type, sort, page, size));
+	}
+
+	@GetMapping("/{gifticonId}")
+	public ResponseEntity<UsedGifticonDetailResponseDto> getUsedGifticon(@PathVariable Integer gifticonId) {
+		return ResponseEntity.ok(gifticonAppService.getUsedGifticonDetail(gifticonId));
 	}
 }
