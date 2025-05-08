ec8da5e0aea6c752c2cd2b6d5946da050a0593bf
조대성 <tfer2442@naver.com>
2025-05-08 12:49:43 +0900

    feat: 기프티콘 저장 요청 DTO 유효성 검증 개선

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonSaveRequestDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonSaveRequestDto.java
@@ -1,12 +1,13 @@
 package com.eurachacha.achacha.application.port.input.gifticon.dto.request;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,17 +15,20 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class GifticonSaveRequestDto implements Serializable {
+public class GifticonSaveRequestDto {
 	@NotBlank(message = "바코드 번호는 필수입니다")
+	@Size(max = 32, message = "바코드 번호는 최대 32자까지 입력 가능합니다")
 	private String gifticonBarcodeNumber;
 
 	@NotNull(message = "브랜드 ID는 필수입니다")
 	private Integer brandId;
 
 	@NotBlank(message = "상품명은 필수입니다")
+	@Size(max = 64, message = "상품명은 최대 64자까지 입력 가능합니다")
 	private String gifticonName;
 
 	@NotNull(message = "유효기간은 필수입니다")
+	@FutureOrPresent(message = "유효기간은 현재 날짜 이후로 설정해야 합니다")
 	private LocalDate gifticonExpiryDate;
 
 	@NotNull(message = "기프티콘 타입은 필수입니다")
