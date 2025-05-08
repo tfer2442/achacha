29013c32206a29c7848f0234b44ed47a72c8a76c
조대성 <tfer2442@naver.com>
2025-05-08 12:52:15 +0900

    feat: 기프티콘 앱 서비스 인터페이스 메서드 시그니처 정의

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonAppService.java
@@ -6,7 +6,6 @@ import com.eurachacha.achacha.application.port.input.gifticon.dto.request.Giftic
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonDetailResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonsResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.GifticonMetadataResponseDto;
-import com.eurachacha.achacha.application.port.input.gifticon.dto.response.GifticonResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.UsedGifticonsResponseDto;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonScopeType;
 import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonSortType;
@@ -16,7 +15,7 @@ import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonUsedSortType;
 public interface GifticonAppService {
 	GifticonMetadataResponseDto extractGifticonMetadata(MultipartFile image, GifticonType gifticonType);
 
-	GifticonResponseDto saveGifticon(GifticonSaveRequestDto requestDto, MultipartFile originalImage,
+	void saveGifticon(GifticonSaveRequestDto requestDto, MultipartFile originalImage,
 		MultipartFile thumbnailImage, MultipartFile barcodeImage);
 
 	AvailableGifticonsResponseDto getAvailableGifticons(GifticonScopeType scope, GifticonType type,
