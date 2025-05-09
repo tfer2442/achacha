a9b626d388861ae6daa0ae157d89fdb9823d41ad
류재문 <woans5970@naver.com>
2025-05-09 21:15:49 +0900

    feat: 기프티콘 사용내역 조회 로직 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonUsageAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonUsageAppService.java
@@ -1,7 +1,10 @@
 package com.eurachacha.achacha.application.port.input.gifticon;
 
 import com.eurachacha.achacha.application.port.input.gifticon.dto.request.AmountGifticonUseRequestDto;
+import com.eurachacha.achacha.application.port.input.gifticon.dto.response.GifticonUsageHistoriesResponseDto;
 
 public interface GifticonUsageAppService {
 	void useAmountGifticon(Integer gifticonId, AmountGifticonUseRequestDto requestDto);
+
+	GifticonUsageHistoriesResponseDto getGifticonUsageHistorys(Integer gifticonId);
 }
