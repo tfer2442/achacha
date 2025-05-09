887d23464fbe3df68b7d01c10ec14dd78615684e
asj7090 <asj7090@gmail.com>
2025-05-09 15:36:58 +0900

    refactor: @RequiredArgsConstructor 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/security/JwtAuthenticationFilter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/security/JwtAuthenticationFilter.java
@@ -18,15 +18,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtTokenProvider jwtTokenProvider;
-
-	public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-		this.jwtTokenProvider = jwtTokenProvider;
-	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
