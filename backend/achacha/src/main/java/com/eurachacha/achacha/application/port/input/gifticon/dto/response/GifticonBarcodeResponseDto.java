12d90772417ca2c19182fcb836212447757daa29
류재문 <woans5970@naver.com>
2025-05-09 09:58:11 +0900

    refactor: dto 명 변경

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/response/GifticonBarcodeResponseDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/response/GifticonBarcodeResponseDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AvailableGifticonBarcodeResponseDto {
+public class GifticonBarcodeResponseDto {
 	private String gifticonBarcodeNumber;
 	private String barcodePath;
 }
