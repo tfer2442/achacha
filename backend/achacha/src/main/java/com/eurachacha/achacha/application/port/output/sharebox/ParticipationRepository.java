8b044860bbd683e5cf1e8b00512c7382d06af1ae
조대성 <tfer2442@naver.com>
2025-05-09 15:36:56 +0900

    feat: ShareBox 생성 시 Participation 기능 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/sharebox/ParticipationRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/sharebox/ParticipationRepository.java
@@ -1,6 +1,9 @@
 package com.eurachacha.achacha.application.port.output.sharebox;
 
+import com.eurachacha.achacha.domain.model.sharebox.Participation;
+
 public interface ParticipationRepository {
 	boolean checkParticipation(Integer userId, Integer shareBoxId);
 
+	Participation save(Participation participation);
 }
