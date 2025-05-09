9aea6bd1fa413636f13729e781c598b998a3fab6
조대성 <tfer2442@naver.com>
2025-05-09 17:48:04 +0900

    feat: 쉐어박스 참여 API 엔드포인트 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/sharebox/ShareBoxController.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/sharebox/ShareBoxController.java
@@ -1,6 +1,7 @@
 package com.eurachacha.achacha.web.sharebox;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.eurachacha.achacha.application.port.input.sharebox.dto.ShareBoxAppService;
 import com.eurachacha.achacha.application.port.input.sharebox.dto.request.ShareBoxCreateRequestDto;
+import com.eurachacha.achacha.application.port.input.sharebox.dto.request.ShareBoxJoinRequestDto;
 import com.eurachacha.achacha.application.port.input.sharebox.dto.response.ShareBoxCreateResponseDto;
 
 import jakarta.validation.Valid;
@@ -20,11 +22,22 @@ public class ShareBoxController {
 
 	private final ShareBoxAppService shareBoxAppService;
 
+	// 쉐어박스 생성 엔드포인트
 	@PostMapping
 	public ResponseEntity<ShareBoxCreateResponseDto> createShareBox(
 		@Valid @RequestBody ShareBoxCreateRequestDto requestDto) {
 
 		ShareBoxCreateResponseDto responseDto = shareBoxAppService.createShareBox(requestDto);
 		return ResponseEntity.ok(responseDto);
+	}
+
+	// 쉐어박스 참여 엔드포인트
+	@PostMapping("/{shareBoxId}/join")
+	public ResponseEntity<String> joinShareBox(
+		@PathVariable Integer shareBoxId,
+		@Valid @RequestBody ShareBoxJoinRequestDto requestDto) {
+
+		shareBoxAppService.joinShareBox(shareBoxId, requestDto);
+		return ResponseEntity.ok("쉐어박스에 성공적으로 참여했습니다.");
 	}
 }
