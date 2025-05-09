1ee34ef72465820575a5847bfba5bd43a4d3cc99
asj7090 <asj7090@gmail.com>
2025-05-09 15:18:11 +0900

    feat: 카카오 로그인 사용자 정보 조회 메서드 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/user/UserRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/user/UserRepository.java
@@ -1,7 +1,14 @@
 package com.eurachacha.achacha.application.port.output.user;
 
+import java.util.Optional;
+
 import com.eurachacha.achacha.domain.model.user.User;
 
 public interface UserRepository {
+	Optional<User> findByProviderAndProviderUserId(String provider, String providerUserId);
+
 	User findById(Integer id);
+
+	User save(User user);
+
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/user/UserJpaRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/user/UserJpaRepository.java
@@ -1,5 +1,7 @@
 package com.eurachacha.achacha.infrastructure.adapter.output.persistence.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.eurachacha.achacha.domain.model.user.User;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByProviderAndProviderUserId(String provider, String providerUserId);
+
 }
