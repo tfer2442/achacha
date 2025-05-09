5ba48513e0f9b54a5045d1ecbca0a62b4305d2b2
asj7090 <asj7090@gmail.com>
2025-05-09 15:11:42 +0900

    feat: 카카오 로그인 및 토큰 갱신 로직 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/auth/AuthAppService.java
@@ -0,0 +1,9 @@
+package com.eurachacha.achacha.application.port.input.auth;
+
+import com.eurachacha.achacha.application.port.input.auth.dto.request.KakaoLoginRequestDto;
+import com.eurachacha.achacha.application.port.input.auth.dto.response.TokenResponseDto;
+
+public interface AuthAppService {
+	TokenResponseDto loginWithKakao(KakaoLoginRequestDto requestDto);
+	TokenResponseDto refreshToken(String refreshToken);
+}
