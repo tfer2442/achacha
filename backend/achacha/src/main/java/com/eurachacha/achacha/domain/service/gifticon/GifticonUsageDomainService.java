6c46b3119b574e98f01549efb2e38063c523841c
류재문 <woans5970@naver.com>
2025-05-10 03:01:26 +0900

    feat: 금액형 타입 & 금액, 사용자 유효성 & 잔액 계산 로직 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainService.java
@@ -1,8 +1,20 @@
 package com.eurachacha.achacha.domain.service.gifticon;
 
 import com.eurachacha.achacha.domain.model.gifticon.Gifticon;
+import com.eurachacha.achacha.domain.model.history.UsageHistory;
 
 public interface GifticonUsageDomainService {
 	// 잔액 여부 확인
 	boolean hasBalance(Gifticon gifticon, Integer usageAmount);
+
+	void validateAmountGifticonType(Gifticon gifticon);
+
+	void validateAmount(Integer newAmount);
+
+	void validateUsageUser(Integer userId, Integer usageUserId);
+
+	int getFindGifticonRemainingAmount(Integer newAmount, UsageHistory findUsageHistory,
+		Gifticon findGifticon);
+
+	int updateUsageHistory(Integer userId, Integer newAmount, Gifticon findGifticon, UsageHistory findUsageHistory);
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainServiceImpl.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainServiceImpl.java
@@ -3,6 +3,10 @@ package com.eurachacha.achacha.domain.service.gifticon;
 import org.springframework.stereotype.Service;
 
 import com.eurachacha.achacha.domain.model.gifticon.Gifticon;
+import com.eurachacha.achacha.domain.model.gifticon.enums.GifticonType;
+import com.eurachacha.achacha.domain.model.history.UsageHistory;
+import com.eurachacha.achacha.web.common.exception.CustomException;
+import com.eurachacha.achacha.web.common.exception.ErrorCode;
 
 @Service
 public class GifticonUsageDomainServiceImpl implements GifticonUsageDomainService {
@@ -12,4 +16,64 @@ public class GifticonUsageDomainServiceImpl implements GifticonUsageDomainServic
 		return usageAmount <= gifticon.getRemainingAmount();
 	}
 
+	@Override // 금액형 타입 검증
+	public void validateAmountGifticonType(Gifticon gifticon) {
+		if (gifticon.getType() != GifticonType.AMOUNT) {
+			throw new CustomException(ErrorCode.INVALID_AMOUNT_GIFTICON_VALUE);
+		}
+	}
+
+	@Override // 금액 유효성 검증 (0 이하)
+	public void validateAmount(Integer newAmount) {
+		if (newAmount <= 0) {
+			throw new CustomException(ErrorCode.INVALID_AMOUNT_VALUE);
+		}
+	}
+
+	@Override // 사용자 유효성 검증 (기존 사용자와 동일한지 확인)
+	public void validateUsageUser(Integer userId, Integer usageUserId) {
+		if (!usageUserId.equals(userId)) {
+			throw new CustomException(ErrorCode.GIFTICON_NO_USAGE_HISTORY);
+		}
+	}
+
+	@Override // 기프티콘 잔액 계산 (잔액이 부족한 경우 예외 발생)
+	public int getFindGifticonRemainingAmount(Integer newAmount, UsageHistory findUsageHistory,
+		Gifticon findGifticon) {
+		int findUsageHistoryUsageAmount = findUsageHistory.getUsageAmount(); //이전 사용 금액
+		int amountDifference = 0; // 변경 금액 차이
+		int findGifticonRemainingAmount = findGifticon.getRemainingAmount(); // 현재 잔액
+
+		// 금액 증가 시 잔액 확인
+		if (newAmount > findUsageHistoryUsageAmount) {
+			amountDifference = newAmount - findUsageHistoryUsageAmount; // 늘어난 차이
+
+			if (findGifticonRemainingAmount < amountDifference) { // 늘어난 차이보다 잔액이 부족한 경우
+				throw new CustomException(ErrorCode.GIFTICON_INSUFFICIENT_BALANCE);
+			}
+
+			findGifticonRemainingAmount -= amountDifference;
+
+		} else if (newAmount < findUsageHistoryUsageAmount) { // 금액 감소 시 잔액 반환
+			amountDifference = findUsageHistoryUsageAmount - newAmount;
+			findGifticonRemainingAmount += amountDifference;
+		}
+		return findGifticonRemainingAmount;
+	}
+
+	@Override
+	public int updateUsageHistory(Integer userId, Integer newAmount, Gifticon findGifticon,
+		UsageHistory findUsageHistory) {
+		// 기프티콘 타입 확인
+		validateAmountGifticonType(findGifticon);
+
+		// 수정하려는 사용자가 기존 사용자와 동일한지 확인
+		validateUsageUser(userId, findUsageHistory.getUser().getId());
+
+		// 변경 금액이 유효한지 확인
+		validateAmount(newAmount);
+
+		// 잔액 계산
+		return getFindGifticonRemainingAmount(newAmount, findUsageHistory, findGifticon);
+	}
 }
