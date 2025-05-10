06f255098a2376695fc0ed0f1fd43efce0dc70fb
ssuzyn <asj7090@gmail.com>
2025-05-11 01:55:58 +0900

    feat: 아차차 알림 설정 엔티티 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/notification/enums/ExpirationCycle.java
@@ -0,0 +1,14 @@
+package com.eurachacha.achacha.domain.model.notification.enums;
+
+/**
+ * 알림 설정의 만료 주기를 정의하는 열거형 클래스
+ */
+public enum ExpirationCycle {
+	ONE_DAY,       // 1일
+	TWO_DAYS,      // 2일
+	THREE_DAYS,    // 3일
+	ONE_WEEK,      // 1주
+	ONE_MONTH,     // 1개월
+	TWO_MONTHS,    // 2개월
+	THREE_MONTHS   // 3개월
+}
