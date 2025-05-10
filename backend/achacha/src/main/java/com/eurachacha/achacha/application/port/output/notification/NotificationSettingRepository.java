b3ae3f407dc9361169136067fa79b2b42c2c8d2f
ssuzyn <asj7090@gmail.com>
2025-05-11 02:24:58 +0900

    feat: 알림 설정 목록 일괄 저장하는 메서드 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/notification/NotificationSettingRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/notification/NotificationSettingRepository.java
@@ -1,9 +1,13 @@
 package com.eurachacha.achacha.application.port.output.notification;
 
+import java.util.List;
+
 import com.eurachacha.achacha.domain.model.notification.NotificationSetting;
 
 public interface NotificationSettingRepository {
 
 	NotificationSetting save(NotificationSetting notificationSetting);
+
+	List<NotificationSetting> saveAll(List<NotificationSetting> notificationSettings);
 
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationSettingPersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationSettingPersistenceAdapter.java
@@ -1,5 +1,7 @@
 package com.eurachacha.achacha.infrastructure.adapter.output.persistence.notification;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.eurachacha.achacha.application.port.output.notification.NotificationSettingRepository;
@@ -16,5 +18,10 @@ public class NotificationSettingPersistenceAdapter implements NotificationSettin
 	@Override
 	public NotificationSetting save(NotificationSetting notificationSetting) {
 		return notificationSettingJpaRepository.save(notificationSetting);
+	}
+
+	@Override
+	public List<NotificationSetting> saveAll(List<NotificationSetting> notificationSettings) {
+		return notificationSettingJpaRepository.saveAll(notificationSettings);
 	}
 }
