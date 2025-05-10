3735975436d2fc91f5fb11a1b8c0b024b29e4665
류재문 <woans5970@naver.com>
2025-05-10 23:40:28 +0900

    feat: 상품형 기프티콘 사용내역 조회 응답 로직 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonUsageAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonUsageAppService.java
@@ -2,15 +2,18 @@ package com.eurachacha.achacha.application.port.input.gifticon;
 
 import com.eurachacha.achacha.application.port.input.gifticon.dto.request.AmountGifticonUseRequestDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AmountGifticonUsageHistoriesResponseDto;
+import com.eurachacha.achacha.application.port.input.gifticon.dto.response.ProductGifticonUsageHistoriesResponseDto;
 
 public interface GifticonUsageAppService {
 	void useAmountGifticon(Integer gifticonId, AmountGifticonUseRequestDto requestDto);
 
-	AmountGifticonUsageHistoriesResponseDto getAmountGifticonUsageHistorys(Integer gifticonId);
+	AmountGifticonUsageHistoriesResponseDto getAmountGifticonUsageHistories(Integer gifticonId);
 
 	void updateGifticonUsageHistory(Integer gifticonId, Integer usageHistoryId, AmountGifticonUseRequestDto requestDto);
 
 	void deleteGifticonUsageHistory(Integer gifticonId, Integer usageHistoryId);
 
 	void useProductGifticon(Integer gifticonId);
+
+	ProductGifticonUsageHistoriesResponseDto getProductGifticonUsageHistories(Integer gifticonId);
 }
