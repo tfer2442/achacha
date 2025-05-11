bc1b10e780b64e24a951061b41d3b8edd1c34694
ssuzyn <asj7090@gmail.com>
2025-05-11 15:29:29 +0900

    유효기간 만료 알림 주기 설정 변경 API 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/notification/NotificationController.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/notification/NotificationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eurachacha.achacha.application.port.input.notification.NotificationSettingAppService;
+import com.eurachacha.achacha.application.port.input.notification.dto.request.ExpirationCycleUpdateDto;
 import com.eurachacha.achacha.application.port.input.notification.dto.request.NotificationSettingUpdateRequestDto;
 import com.eurachacha.achacha.application.port.input.notification.dto.response.NotificationSettingDto;
 import com.eurachacha.achacha.domain.model.notification.enums.NotificationTypeCode;
@@ -38,5 +39,12 @@ public class NotificationController {
 		notificationSettingAppService.updateNotificationSetting(notificationTypeCode, request.getIsEnabled());
 
 		return ResponseEntity.ok("알림 허용 설정 변경 성공");
+	}
+
+	@PatchMapping("/expirationCycle")
+	public ResponseEntity<?> updateExpirationCycle(@RequestBody ExpirationCycleUpdateDto expirationCycleUpdateDto) {
+		notificationSettingAppService.updateExpirationCycle(expirationCycleUpdateDto.getExpirationCycle());
+
+		return ResponseEntity.ok("알림 주기 변경 성공");
 	}
 }
