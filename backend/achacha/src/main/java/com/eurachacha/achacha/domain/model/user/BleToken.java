5c3e8eaa08d0d85d8a57a03d409d2b18823d3672
류재문 <woans5970@naver.com>
2025-05-11 18:35:43 +0900

    feat: BleToken 엔티티 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/user/BleToken.java
@@ -0,0 +1,51 @@
+package com.eurachacha.achacha.domain.model.user;
+
+import java.time.LocalDateTime;
+
+import com.eurachacha.achacha.domain.model.common.TimeStampEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class BleToken extends TimeStampEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false, unique = true)
+	private User user;
+
+	@Column(nullable = false, unique = true, length = 16)
+	private String token;
+
+	@Column(nullable = false)
+	private LocalDateTime expiresAt;
+
+	// 유효기간 만료 여부
+	public boolean isExpired() {
+		return LocalDateTime.now().isBefore(expiresAt);
+	}
+
+	public void updateToken(String token, LocalDateTime expiresAt) {
+		this.token = token;
+		this.expiresAt = expiresAt;
+	}
+}
