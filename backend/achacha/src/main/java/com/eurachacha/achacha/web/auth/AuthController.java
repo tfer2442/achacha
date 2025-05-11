9b82ae0ea7b2415982c00f3022e81bd650d2ef9d
류재문 <woans5970@naver.com>
2025-05-11 22:02:14 +0900

    refactor: ble 발급 api 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/auth/AuthController.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/auth/AuthController.java
@@ -1,11 +1,17 @@
 package com.eurachacha.achacha.web.auth;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.eurachacha.achacha.application.port.input.auth.AuthAppService;
+import com.eurachacha.achacha.application.port.input.auth.dto.request.BleTokenRequestDto;
 import com.eurachacha.achacha.application.port.input.auth.dto.request.KakaoLoginRequestDto;
 import com.eurachacha.achacha.application.port.input.auth.dto.request.RefreshTokenRequestDto;
 import com.eurachacha.achacha.application.port.input.auth.dto.response.TokenResponseDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.eurachacha.achacha.application.port.output.auth.dto.response.BleTokenResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,4 +32,10 @@ public class AuthController {
 		TokenResponseDto tokenResponseDto = authAppService.refreshToken(requestDto);
 		return ResponseEntity.ok(tokenResponseDto);
 	}
+
+	@PostMapping("/ble")
+	public ResponseEntity<BleTokenResponseDto> generateBleToken(@RequestBody BleTokenRequestDto request) {
+		return ResponseEntity.ok(authAppService.generateBleToken(request.getBleTokenValue()));
+	}
+
 }
