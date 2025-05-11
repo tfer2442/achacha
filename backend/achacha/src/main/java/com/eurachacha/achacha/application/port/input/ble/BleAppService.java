8b38133e748a59ff447a0c372b156733c3428630
류재문 <woans5970@naver.com>
2025-05-11 23:09:48 +0900

    refactor: 매개변수명 변경

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/ble/BleAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/ble/BleAppService.java
@@ -3,5 +3,5 @@ package com.eurachacha.achacha.application.port.input.ble;
 import com.eurachacha.achacha.application.port.input.ble.dto.response.BleTokenResponseDto;
 
 public interface BleAppService {
-	BleTokenResponseDto generateBleToken(String tokenValue);
+	BleTokenResponseDto generateBleToken(String value);
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/ble/BleTokenRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/ble/BleTokenRepository.java
@@ -3,9 +3,9 @@ package com.eurachacha.achacha.application.port.output.ble;
 import com.eurachacha.achacha.domain.ble.BleToken;
 
 public interface BleTokenRepository {
-	void deleteByUserIdAndValue(Integer userId, String tokenValue);
+	void deleteByUserIdAndValue(Integer userId, String value);
 
 	BleToken save(BleToken bleToken);
 
-	boolean existsByValue(String token);
+	boolean existsByValue(String value);
 }
