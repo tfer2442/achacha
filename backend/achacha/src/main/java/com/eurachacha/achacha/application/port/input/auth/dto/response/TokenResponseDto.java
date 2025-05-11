58c841cae6f5aa74520c5df4ec65a7c5e4cce2b0
류재문 <woans5970@naver.com>
2025-05-11 21:58:52 +0900

    refactor: 로그인 시 반환되는 dto 필드 제거

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/auth/dto/response/TokenResponseDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/auth/dto/response/TokenResponseDto.java
@@ -1,7 +1,5 @@
 package com.eurachacha.achacha.application.port.input.auth.dto.response;
 
-import java.time.LocalDateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,6 +9,4 @@ public class TokenResponseDto {
 	private String accessToken;
 	private String refreshToken;
 	private long expiresIn;
-	private String bleToken;
-	private LocalDateTime bleTokenExpiresAt;
 }
