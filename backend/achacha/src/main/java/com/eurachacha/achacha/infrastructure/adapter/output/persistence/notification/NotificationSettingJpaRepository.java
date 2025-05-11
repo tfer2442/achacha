0b4d29eb58ff9722a1e0c65c4cc9ab25f80c48c2
ssuzyn <asj7090@gmail.com>
2025-05-11 12:58:16 +0900

    feat: 사용자 ID로 모든 알림 설정을 조회하는 메서드 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationSettingJpaRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationSettingJpaRepository.java
@@ -1,8 +1,14 @@
 package com.eurachacha.achacha.infrastructure.adapter.output.persistence.notification;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.eurachacha.achacha.domain.model.notification.NotificationSetting;
 
 public interface NotificationSettingJpaRepository extends JpaRepository<NotificationSetting, Integer> {
+	// 사용자 ID로 모든 알림 설정을 조회
+	@Query("SELECT ns FROM NotificationSetting ns JOIN FETCH ns.notificationType WHERE ns.user.id = :userId")
+	List<NotificationSetting> findAllByUserIdWithType(Integer userId);
 }
