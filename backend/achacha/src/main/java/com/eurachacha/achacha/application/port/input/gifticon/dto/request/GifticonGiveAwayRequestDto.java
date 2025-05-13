b466d2023ad7838d1ac3e92a205388c45b773608
류재문 <woans5970@naver.com>
2025-05-13 13:40:41 +0900

    feat: 기프티콘 뿌리기 요청 dto 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonGiveAwayRequestDto.java
@@ -0,0 +1,14 @@
+package com.eurachacha.achacha.application.port.input.gifticon.dto.request;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GifticonGiveAwayRequestDto {
+	private List<String> userIds;
+}
