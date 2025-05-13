e0d7222fd937687062e7336e44e798a183df39a6
류재문 <woans5970@naver.com>
2025-05-14 00:47:01 +0900

    refactor: 클래스명 변경

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonGiveAppService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/GifticonGiveAppService.java
@@ -2,6 +2,6 @@ package com.eurachacha.achacha.application.port.input.gifticon;
 
 import java.util.List;
 
-public interface GifticonSharingAppService {
+public interface GifticonGiveAppService {
 	void giveAwayGifticon(Integer gifticonId, List<String> uuids);
 }
