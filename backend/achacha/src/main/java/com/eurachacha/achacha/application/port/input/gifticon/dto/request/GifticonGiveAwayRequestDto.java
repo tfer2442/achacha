bab1e03ce28acba2eb9c854878a9da5e98d30b06
류재문 <woans5970@naver.com>
2025-05-13 22:47:57 +0900

    feat: 요청 파라미터 명 변경

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonGiveAwayRequestDto.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/gifticon/dto/request/GifticonGiveAwayRequestDto.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GifticonGiveAwayRequestDto {
-	private List<String> userIds;
+	private List<String> uuids;
 }
