788fd0d29294eacb4991ca6b1e0828f3199e3ad7
조대성 <tfer2442@naver.com>
2025-05-13 21:14:16 +0900

    feat: 쉐어박스 목록 조회를 위한 응답 DTO 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/sharebox/ShareBoxAppService.java
@@ -0,0 +1,21 @@
+package com.eurachacha.achacha.application.port.input.sharebox;
+
+import com.eurachacha.achacha.application.port.input.sharebox.dto.request.ShareBoxCreateRequestDto;
+import com.eurachacha.achacha.application.port.input.sharebox.dto.request.ShareBoxJoinRequestDto;
+import com.eurachacha.achacha.application.port.input.sharebox.dto.response.ShareBoxCreateResponseDto;
+import com.eurachacha.achacha.application.port.input.sharebox.dto.response.ShareBoxesResponseDto;
+import com.eurachacha.achacha.domain.model.sharebox.enums.ShareBoxSortType;
+
+public interface ShareBoxAppService {
+	ShareBoxCreateResponseDto createShareBox(ShareBoxCreateRequestDto requestDto);
+
+	void joinShareBox(Integer shareBoxId, ShareBoxJoinRequestDto requestDto);
+
+	void shareGifticon(Integer shareBoxId, Integer gifticonId);
+
+	void unshareGifticon(Integer shareBoxId, Integer gifticonId);
+
+	ShareBoxesResponseDto getShareBoxes(ShareBoxSortType sort, Integer page, Integer size);
+
+	ShareBoxesResponseDto getShareBoxes(String sort, Integer page, Integer size);
+}
