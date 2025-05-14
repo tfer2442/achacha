481c82fcb5b86dddbac58f37644e1892ba881807
류재문 <woans5970@naver.com>
2025-05-14 10:43:18 +0900

    refactor: 목적이 동일한 검증 메서드 병합

--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainService.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/gifticon/GifticonUsageDomainService.java
@@ -4,8 +4,6 @@ import com.eurachacha.achacha.domain.model.gifticon.Gifticon;
 import com.eurachacha.achacha.domain.model.history.UsageHistory;
 
 public interface GifticonUsageDomainService {
-	// 잔액 여부 확인
-	void validateBalance(Gifticon gifticon, Integer usageAmount);
 
 	int getFindGifticonRemainingAmount(Integer newAmount, UsageHistory findUsageHistory,
 		Gifticon findGifticon);
