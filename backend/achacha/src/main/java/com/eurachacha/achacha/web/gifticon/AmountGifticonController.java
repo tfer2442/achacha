af6ef6c917a28d9ddf6cc0f7c957e74cf340dbb1
류재문 <woans5970@naver.com>
2025-05-14 13:45:25 +0900

    refactor: 상품형, 금액형 기프티콘 사용 응답 반환 원복

--- backend/achacha/src/main/java/com/eurachacha/achacha/web/gifticon/AmountGifticonController.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/gifticon/AmountGifticonController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.eurachacha.achacha.application.port.input.gifticon.GifticonUsageAppService;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.request.AmountGifticonUseRequestDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AmountGifticonUsageHistoriesResponseDto;
-import com.eurachacha.achacha.application.port.input.gifticon.dto.response.GifticonMessageResponseDto;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,12 +25,12 @@ public class AmountGifticonController {
 	private final GifticonUsageAppService gifticonUsageAppService;
 
 	@PostMapping("/{gifticonId}/use")
-	public ResponseEntity<GifticonMessageResponseDto> useGifticon(
+	public ResponseEntity<String> useGifticon(
 		@PathVariable Integer gifticonId,
 		@Valid @RequestBody AmountGifticonUseRequestDto requestDto
 	) {
 		gifticonUsageAppService.useAmountGifticon(gifticonId, requestDto);
-		return ResponseEntity.ok(new GifticonMessageResponseDto("기프티콘이 사용되었습니다."));
+		return ResponseEntity.ok("기프티콘이 사용되었습니다.");
 	}
 
 	@GetMapping("/{gifticonId}/usage-history")
