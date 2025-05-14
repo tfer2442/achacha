f6682bc4b8b07edf8d087515cd9ea1fbe3e5ec35
ssuzyn <asj7090@gmail.com>
2025-05-14 19:52:01 +0900

    refactor: 템플릿의 색상이 없는 경우 빈배열로 반환하도록 수정

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/present/ColorPalettePersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/present/ColorPalettePersistenceAdapter.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 
 import com.eurachacha.achacha.application.port.output.present.ColorPaletteRepository;
 import com.eurachacha.achacha.domain.model.present.ColorPalette;
-import com.eurachacha.achacha.web.common.exception.CustomException;
-import com.eurachacha.achacha.web.common.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,10 +17,6 @@ public class ColorPalettePersistenceAdapter implements ColorPaletteRepository {
 
 	@Override
 	public List<ColorPalette> findByPresentTemplateId(Integer templateId) {
-		List<ColorPalette> colorPalettes = colorPaletteJpaRepository.findByPresentTemplateId(templateId);
-		if(colorPalettes.isEmpty()){
-			throw new CustomException(ErrorCode.COLOR_NOT_FOUND);
-		}
-		return colorPalettes;
+		return colorPaletteJpaRepository.findByPresentTemplateId(templateId);
 	}
 }
