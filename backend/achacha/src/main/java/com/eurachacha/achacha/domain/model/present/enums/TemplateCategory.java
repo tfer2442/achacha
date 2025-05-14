1d341f5f166a9849db9a708ba2500987ccc68bca
asj7090 <asj7090@gmail.com>
2025-05-14 15:26:08 +0900

    feat: 선물 카드 템플릿 종류 enum 클래스 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/present/enums/TemplateCategory.java
@@ -0,0 +1,9 @@
+package com.eurachacha.achacha.domain.model.present.enums;
+
+public enum TemplateCategory {
+	GENERAL, // 색상 디자인
+	ACHACHA, // 아차차 디자인
+	AMULET,  // 부적 디자인
+	PICKUP,  // 픽업 디자인
+	CLOVER   // 클로버 디자인
+}
