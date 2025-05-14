fbbdf07bffd420494155b749563f792e97a3e0f4
ssuzyn <asj7090@gmail.com>
2025-05-15 02:27:53 +0900

    chore: 빌드 출력 디렉토리 지정

--- frontend_web/achacha/vite.config.js
+++ frontend_web/achacha/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 빌드 출력 디렉토리를 'build'로 설정
+  },
 })
