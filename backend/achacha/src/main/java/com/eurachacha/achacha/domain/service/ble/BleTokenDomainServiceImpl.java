eefdc5ce344ed154cb9b879dd13175d856913570
류재문 <woans5970@naver.com>
2025-05-15 01:01:36 +0900

    refactor: 토큰 길이 상수화

--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/ble/BleTokenDomainServiceImpl.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/service/ble/BleTokenDomainServiceImpl.java
@@ -11,8 +11,11 @@ public class BleTokenDomainServiceImpl implements BleTokenDomainService {
 	// 토큰 생성에 사용할 문자셋
 	private static final String CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	// 기본 토큰 길이 - 8바이트(64비트)
+	// 기본 토큰 크기 - 8바이트(64비트)
 	private static final int TOKEN_BYTES = 8;
+
+	// 기본 토큰 길이 - 9자리
+	private static final int TOKEN_LENGTH = 9;
 
 	// 랜덤 토큰 생성 및 Base62 인코딩
 	@Override
@@ -35,13 +38,13 @@ public class BleTokenDomainServiceImpl implements BleTokenDomainService {
 		StringBuilder result = new StringBuilder(sb.reverse().toString());
 
 		// 길이가 9자리보다 짧은 경우 패딩 추가
-		while (result.length() < 9) {
+		while (result.length() < TOKEN_LENGTH) {
 			result.insert(0, CHARS.charAt(0));
 		}
 
 		// 길이가 9자리보다 긴 경우 잘라내기
-		if (result.length() > 9) {
-			result = new StringBuilder(result.substring(0, 9));
+		if (result.length() > TOKEN_LENGTH) {
+			result = new StringBuilder(result.substring(0, TOKEN_LENGTH));
 		}
 
 		return result.toString();
