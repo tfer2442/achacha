8f15de221ae2c5b2de8fc7b516c83fc0ca2c7c27
조대성 <tfer2442@naver.com>
2025-05-14 13:30:17 +0900

    feat: 쉐어박스 삭제 기능 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/sharebox/ShareBoxJpaRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/sharebox/ShareBoxJpaRepository.java
@@ -13,13 +13,21 @@ import com.eurachacha.achacha.domain.model.sharebox.ShareBox;
 
 @Repository
 public interface ShareBoxJpaRepository extends JpaRepository<ShareBox, Integer>, ShareBoxRepositoryCustom {
-	@Query("SELECT s FROM ShareBox s JOIN FETCH s.user WHERE s.id = :id")
+	@Query("""
+		SELECT s FROM ShareBox s
+		JOIN FETCH s.user
+		WHERE s.id = :id
+		""")
 	Optional<ShareBox> findByIdWithUser(@Param("id") Integer id);
 
 	boolean existsByInviteCode(String inviteCode);
 
 	Optional<ShareBox> findByInviteCode(String inviteCode);
 
-	@Query("SELECT p FROM Participation p JOIN FETCH p.user WHERE p.sharebox.id = :shareBoxId")
+	@Query("""
+		SELECT p FROM Participation p
+		JOIN FETCH p.user
+		WHERE p.sharebox.id = :shareBoxId
+		""")
 	List<Participation> findParticipationsByShareBoxId(@Param("shareBoxId") Integer shareBoxId);
 }
