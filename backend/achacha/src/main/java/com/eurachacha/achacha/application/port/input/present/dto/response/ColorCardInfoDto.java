1f9d027ce613cc4e62fde920abc5f19b01a22bb2
asj7090 <asj7090@gmail.com>
2025-05-15 14:12:49 +0900

    feat: 색상별 카드 정보 DTO 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/present/dto/response/ColorCardInfoDto.java
@@ -0,0 +1,19 @@
+package com.eurachacha.achacha.application.port.input.present.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ColorCardInfoDto {
+	private Integer colorPaletteId;
+	private String cardImagePath;
+}
