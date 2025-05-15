f2f10b2818a665a6cf3dc44f0c85e6a02d0f77c8
류재문 <woans5970@naver.com>
2025-05-15 16:32:36 +0900

    refactor: 사용가능 기프티콘 목록 조회 메서드 반환타입 수정

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/gifticon/GifticonPersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/gifticon/GifticonPersistenceAdapter.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
-import com.eurachacha.achacha.application.port.input.gifticon.dto.response.AvailableGifticonResponseDto;
 import com.eurachacha.achacha.application.port.input.gifticon.dto.response.UsedGifticonResponseDto;
 import com.eurachacha.achacha.application.port.output.gifticon.GifticonRepository;
 import com.eurachacha.achacha.domain.model.gifticon.Gifticon;
@@ -31,8 +30,8 @@ public class GifticonPersistenceAdapter implements GifticonRepository {
 	}
 
 	@Override
-	public Slice<AvailableGifticonResponseDto> findAvailableGifticons(Integer userId, GifticonScopeType scope,
-		GifticonType type, Pageable pageable) {
+	public Slice<Gifticon> findAvailableGifticons(Integer userId, GifticonScopeType scope, GifticonType type,
+		Pageable pageable) {
 		return gifticonJpaRepository.findAvailableGifticons(userId, scope, type, pageable);
 	}
 
