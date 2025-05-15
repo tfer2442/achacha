fceea28697db231b97a928dafb085bb54c066caf
조대성 <tfer2442@naver.com>
2025-05-15 15:03:21 +0900

    chore: 프로젝트 초기 세팅

---
+++ backend-notification/achacha-notification/src/main/java/com/eurachacha/achacha/notification/AchachaNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.eurachacha.achacha.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchachaNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchachaNotificationApplication.class, args);
+	}
+
+}
