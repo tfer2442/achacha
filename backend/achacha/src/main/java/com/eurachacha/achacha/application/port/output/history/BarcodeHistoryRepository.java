17782eef57dabd5b812efd1738d797043da8da3c
류재문 <woans5970@naver.com>
2025-05-16 17:11:57 +0900

    refactor: 바코드 내역 관련 로직 삭제

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/history/BarcodeHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eurachacha.achacha.application.port.output.history;
-
-import com.eurachacha.achacha.domain.model.history.BarcodeHistory;
-
-public interface BarcodeHistoryRepository {
-	BarcodeHistory saveBarcodeHistory(BarcodeHistory barcodeHistory);
-}
