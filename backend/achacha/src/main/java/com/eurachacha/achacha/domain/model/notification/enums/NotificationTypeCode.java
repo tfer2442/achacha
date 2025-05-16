2e3dd5fe6e631236ca92c04372919f797c319bc7
asj7090 <asj7090@gmail.com>
2025-05-16 17:30:47 +0900

    refactor: 알림 타입 코드의 displayName 수정

--- backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/notification/enums/NotificationTypeCode.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/domain/model/notification/enums/NotificationTypeCode.java
@@ -9,14 +9,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum NotificationTypeCode {
-	LOCATION_BASED("근접 매장 알림"),
+	LOCATION_BASED("주변 매장 알림"),
 	EXPIRY_DATE("유효기간 만료 알림"),
-	RECEIVE_GIFTICON("선물 뿌리기 알림"),
+	RECEIVE_GIFTICON("기프티콘 뿌리기 수신"),
 	USAGE_COMPLETE("사용완료 여부 알림"),
-	SHAREBOX_GIFTICON("쉐어박스 기프티콘 등록 알림"),
-	SHAREBOX_USAGE_COMPLETE("쉐어박스 기프티콘 사용 알림"),
-	SHAREBOX_MEMBER_JOIN("쉐어박스 멤버 참여 알림"),
-	SHAREBOX_DELETED("쉐어박스 그룹 삭제 알림");
+	SHAREBOX_GIFTICON("쉐어박스 기프티콘 등록"),
+	SHAREBOX_USAGE_COMPLETE("쉐어박스 기프티콘 사용"),
+	SHAREBOX_MEMBER_JOIN("쉐어박스 멤버 참여"),
+	SHAREBOX_DELETED("쉐어박스 그룹 삭제");
 
 	private final String displayName;
 
