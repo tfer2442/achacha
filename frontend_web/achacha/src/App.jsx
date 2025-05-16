2be9e3290b020eee6fd1b9c498f9e780369f6e9c
Parkjunsu <koup28@naver.com>
2025-05-16 14:42:27 +0900

    feat: 선물하기 메시지 연결

--- frontend_web/achacha/src/App.jsx
+++ frontend_web/achacha/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import KakaoBridge from './components/KakaoBridge';
 import KakaoShare from './components/KakaoShare';
+import PresentShare from './components/PresentShare';
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/kakao-bridge" element={<KakaoBridge />} />
         <Route path="/kakao-share" element={<KakaoShare />} />
+        <Route path="/present-share" element={<PresentShare />} />
       </Routes>
     </Router>
   );
