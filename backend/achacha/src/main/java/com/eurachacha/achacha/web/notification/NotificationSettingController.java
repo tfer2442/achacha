8bcae13b29a657ba0bbeafd04bb4f42646febf98
ssuzyn <asj7090@gmail.com>
2025-05-17 02:47:25 +0900

    feat: 알림 설정 전용 컨트롤러 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/web/notification/NotificationSettingController.java
@@ -0,0 +1,51 @@
+package com.eurachacha.achacha.web.notification;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eurachacha.achacha.application.port.input.notification.NotificationSettingAppService;
+import com.eurachacha.achacha.application.port.input.notification.dto.request.ExpirationCycleUpdateRequestDto;
+import com.eurachacha.achacha.application.port.input.notification.dto.request.NotificationSettingUpdateRequestDto;
+import com.eurachacha.achacha.application.port.input.notification.dto.response.NotificationSettingDto;
+import com.eurachacha.achacha.domain.model.notification.enums.NotificationTypeCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api/notification-settings")
+@RestController
+@RequiredArgsConstructor
+public class NotificationSettingController {
+
+	private final NotificationSettingAppService notificationSettingAppService;
+
+	@GetMapping
+	public ResponseEntity<List<NotificationSettingDto>> getUserNotificationSettings() {
+		List<NotificationSettingDto> settings = notificationSettingAppService.getUserNotificationSettings();
+		return ResponseEntity.ok(settings);
+	}
+
+	@PatchMapping("/types/{type}")
+	public ResponseEntity<?> updateNotificationSetting(
+		@PathVariable("type") NotificationTypeCode notificationTypeCode,
+		@RequestBody NotificationSettingUpdateRequestDto request) {
+
+		notificationSettingAppService.updateNotificationSetting(notificationTypeCode, request.getIsEnabled());
+
+		return ResponseEntity.ok("알림 허용 설정 변경 성공");
+	}
+
+	@PatchMapping("/expirationCycle")
+	public ResponseEntity<?> updateExpirationCycle(
+		@RequestBody ExpirationCycleUpdateRequestDto expirationCycleUpdateDto) {
+		notificationSettingAppService.updateExpirationCycle(expirationCycleUpdateDto.getExpirationCycle());
+
+		return ResponseEntity.ok("알림 주기 변경 성공");
+	}
+}
