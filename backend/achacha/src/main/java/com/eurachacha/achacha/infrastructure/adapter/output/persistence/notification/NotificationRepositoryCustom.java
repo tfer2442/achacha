c7d37651db5adf6b522e659d52a4fd80a0aadbbf
ssuzyn <asj7090@gmail.com>
2025-05-17 02:45:21 +0900

    feat: 알림 내역 페이징 조회 기능 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationRepositoryCustom.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationRepositoryCustom.java
@@ -1,12 +1,10 @@
-package com.eurachacha.achacha.application.port.output.notification;
+package com.eurachacha.achacha.infrastructure.adapter.output.persistence.notification;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import com.eurachacha.achacha.domain.model.notification.Notification;
 
-public interface NotificationRepository {
-	Notification save(Notification notification);
-
+public interface NotificationRepositoryCustom {
 	Slice<Notification> findNotifications(Integer userId, Pageable pageable);
 }
