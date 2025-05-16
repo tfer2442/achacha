2fc2f1878721c443c17a8ac9523549dc1c1a24e2
조대성 <tfer2442@naver.com>
2025-05-16 09:37:04 +0900

    refactor: 알림 서버 분리로 인한 필요 없는 ignore 삭제

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/port/output/fcm/FcmServicePort.java
+++
@@ -1,6 +0,0 @@
-package com.eurachacha.achacha.application.port.output.fcm;
-
-public interface FcmServicePort {
-	// FCMToken에 해당하는 기기에 FCM 발송
-	void sendNotification(String fcmToken, String title, String body);
-}
