cfa3b5fcfe5b18b17d370f033f0481c77ca6e5ec
조대성 <tfer2442@naver.com>
2025-05-16 16:06:11 +0900

    feat: 알림 서버 기본 구조 및 설정 파일 수정

--- backend-notification/achacha-notification/src/main/java/com/eurachacha/achacha/notification/AchachaNotificationApplication.java
+++ backend-notification/achacha-notification/src/main/java/com/eurachacha/achacha/notification/AchachaNotificationApplication.java
@@ -2,8 +2,12 @@ package com.eurachacha.achacha.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.eurachacha.achacha.notification.config.FirebaseProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FirebaseProperties.class})
 public class AchachaNotificationApplication {
 
 	public static void main(String[] args) {
