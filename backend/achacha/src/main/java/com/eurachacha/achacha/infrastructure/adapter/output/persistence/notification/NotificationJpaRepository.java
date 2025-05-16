add2e24402b4bf2ae9692e8a5a14d5604c0a95bc
asj7090 <asj7090@gmail.com>
2025-05-16 17:05:52 +0900

    feat: 알림내역 저장 기능 구현

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/notification/NotificationJpaRepository.java
@@ -0,0 +1,10 @@
+package com.eurachacha.achacha.infrastructure.adapter.output.persistence.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eurachacha.achacha.domain.model.notification.Notification;
+
+@Repository
+public interface NotificationJpaRepository extends JpaRepository<Notification, Integer> {
+}
