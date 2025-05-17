820fb1deddb06bcb056a7d4233e97bff0bda5dd1
류재문 <woans5970@naver.com>
2025-05-18 01:40:38 +0900

    feat: 알림 요청 dto 추가

---
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/port/input/notification/dto/request/LocationBasedNotificationRequestDto.java
@@ -0,0 +1,12 @@
+package com.eurachacha.achacha.application.port.input.notification.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationBasedNotificationRequestDto {
+	private Integer gifticonId;
+}
