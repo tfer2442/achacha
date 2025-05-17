2799ff3fb8a84b9c0aae34aba68d49b79da06e37
신해인 <godls0215@naver.com>
2025-05-18 00:01:54 +0900

    fix: api 명세 변경에 따른 config 수정

--- src/api/config.js
+++ src/api/config.js
@@ -68,8 +68,8 @@ export const API_CONFIG = {
 
     // 알림 설정 관련 엔드포인트
     NOTIFICATION_SETTINGS: '/api/notification-settings', // 알림 설정 조회
-    NOTIFICATION_SETTINGS_TYPE: type => `/api/notifications-settings/types/${type}`, // 알림 타입별 on/off 설정
-    NOTIFICATION_SETTINGS_EXPIRATION_CYCLE: '/api/notifications-settings/expiration-cycle', // 알림 주기 설정
+    NOTIFICATION_SETTINGS_TYPE: type => `/api/notification-settings/types/${type}`, // 알림 타입별 on/off 설정
+    NOTIFICATION_SETTINGS_EXPIRATION_CYCLE: '/api/notification-settings/expiration-cycle', // 알림 주기 설정
 
     // 알림 내역 관련 엔드포인트
     NOTIFICATIONS: '/api/notifications', // 알림 내역 목록 조회
