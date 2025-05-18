e88c7b7f57ad628894ef5f0936cce2482e1e4a8a
ssuzyn <asj7090@gmail.com>
2025-05-18 16:46:21 +0900

    feat: fcm 토큰 삭제 기능 구현

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/fcm/FcmTokenJpaRepository.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/fcm/FcmTokenJpaRepository.java
@@ -3,6 +3,9 @@ package com.eurachacha.achacha.infrastructure.adapter.output.persistence.fcm;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.eurachacha.achacha.domain.model.fcm.FcmToken;
@@ -11,4 +14,8 @@ import com.eurachacha.achacha.domain.model.fcm.FcmToken;
 public interface FcmTokenJpaRepository extends JpaRepository<FcmToken, Integer> {
 
 	List<FcmToken> findAllByUser_Id(Integer userId);
+
+	@Modifying(clearAutomatically = true)
+	@Query("DELETE FROM FcmToken f WHERE f.user.id = :userId AND f.value = :value")
+	void deleteByUserIdAndValue(@Param("userId") Integer userId, @Param("value") String value);
 }
--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/fcm/FcmTokenPersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/fcm/FcmTokenPersistenceAdapter.java
@@ -3,6 +3,7 @@ package com.eurachacha.achacha.infrastructure.adapter.output.persistence.fcm;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eurachacha.achacha.application.port.output.user.FcmTokenRepository;
 import com.eurachacha.achacha.domain.model.fcm.FcmToken;
@@ -24,5 +25,11 @@ public class FcmTokenPersistenceAdapter implements FcmTokenRepository {
 	@Override
 	public FcmToken save(FcmToken fcmToken) {
 		return fcmTokenJpaRepository.save(fcmToken);
+	}
+
+	@Transactional
+	@Override
+	public void deleteByUserIdAndValue(Integer userId, String value) {
+		fcmTokenJpaRepository.deleteByUserIdAndValue(userId, value);
 	}
 }
