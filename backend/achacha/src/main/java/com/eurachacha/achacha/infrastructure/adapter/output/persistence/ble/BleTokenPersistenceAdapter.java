ac2924bef6c9dba7f629d5ca4ded75658947245d
ssuzyn <asj7090@gmail.com>
2025-05-19 01:09:31 +0900

    refactor: 트랜잭션 어노테이션 삭제

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/ble/BleTokenPersistenceAdapter.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/output/persistence/ble/BleTokenPersistenceAdapter.java
@@ -3,7 +3,6 @@ package com.eurachacha.achacha.infrastructure.adapter.output.persistence.ble;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.eurachacha.achacha.application.port.output.ble.BleTokenRepository;
 import com.eurachacha.achacha.domain.model.ble.BleToken;
@@ -16,7 +15,6 @@ public class BleTokenPersistenceAdapter implements BleTokenRepository {
 
 	private final BleTokenJpaRepository bleTokenJpaRepository;
 
-	@Transactional
 	@Override
 	public void deleteByUserIdAndValue(Integer userId, String value) {
 		bleTokenJpaRepository.deleteByUserIdAndValue(userId, value);
