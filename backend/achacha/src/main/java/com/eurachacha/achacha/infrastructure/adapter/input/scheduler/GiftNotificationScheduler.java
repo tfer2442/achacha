fc91bbd871c3159a90d0c7be7eb60743c9803f68
류재문 <woans5970@naver.com>
2025-05-19 10:12:23 +0900

    refactor: 실제 서비스 시간 추가

--- backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/input/scheduler/GiftNotificationScheduler.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/infrastructure/adapter/input/scheduler/GiftNotificationScheduler.java
@@ -13,6 +13,7 @@ public class GiftNotificationScheduler {
 
 	private final GifticonExpiryNotificationAppService gifticonExpiryNotificationAppService;
 
+	// @Scheduled(cron = "0 0 9 * * *") // 기존 반복 시간 (매일 아침 9시)
 	@Scheduled(cron = "0 0 */1 * * *")
 	public void expirationScheduler() {
 		gifticonExpiryNotificationAppService.sendExpiryDateNotification();
