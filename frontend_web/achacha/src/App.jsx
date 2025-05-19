01f33565d8b617bd1ff1030738f264480c4d02ea
신해인 <godls0215@naver.com>
2025-05-19 14:53:27 +0900

    feat: axios 추가 및 PresentCard 컴포넌트 개선, PresentApi에서 API 응답 데이터 구조화

--- frontend_web/achacha/src/App.jsx
+++ frontend_web/achacha/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import KakaoBridge from './components/KakaoBridge';
 import KakaoShare from './components/KakaoShare';
 import PresentShare from './components/PresentShare';
+import PresentPage from './page/PresentPage';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/kakao-bridge" element={<KakaoBridge />} />
         <Route path="/kakao-share" element={<KakaoShare />} />
         <Route path="/present-share" element={<PresentShare />} />
+        <Route path="/present/:presentCardCode" element={<PresentPage />} />
       </Routes>
     </Router>
   );
