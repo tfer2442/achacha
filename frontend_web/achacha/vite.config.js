7e597db531e3f6066e0ce002b841cef48dc65050
신해인 <godls0215@naver.com>
2025-05-19 17:44:17 +0900

    feat: Vite 프록시 설정 추가 및 PresentApi, PresentCard, PresentPage에서 HTML 응답 처리 로직 개선

--- frontend_web/achacha/vite.config.js
+++ frontend_web/achacha/vite.config.js
@@ -7,4 +7,15 @@ export default defineConfig({
   build: {
     outDir: 'build', // 빌드 출력 디렉토리를 'build'로 설정
   },
+  server: {
+    proxy: {
+      // API 요청을 백엔드 서버로 프록시
+      '/api': {
+        target: 'https://k12d205.p.ssafy.io/', // 백엔드 서버 주소
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path
+      }
+    }
+  }
 })
