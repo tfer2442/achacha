3673a0833522a876df0f796d598784c01061b39d
신해인 <godls0215@naver.com>
2025-05-21 13:00:56 +0900

    fix: 기프티콘 수에 따른 아이콘 선택 로직 수정 (1~3개 아이콘 선택)

--- src/screens/gifticon-share-box/BoxMainScreen.js
+++ src/screens/gifticon-share-box/BoxMainScreen.js
@@ -52,9 +52,9 @@ const MATERIAL_ICONS = {
 
 // 기프티콘 수에 따른 아이콘 선택 함수
 const getShareBoxIcon = count => {
-  if (count >= 1 && count <= 10) {
+  if (count >= 1 && count <= 3) {
     return require('../../assets/images/share_box_icon1.png');
-  } else if (count >= 11 && count <= 20) {
+  } else if (count >= 3 && count <= 5) {
     return require('../../assets/images/share_box_icon2.png');
   } else if (count < 1) {
     return require('../../assets/images/share_box_icon0.png');
