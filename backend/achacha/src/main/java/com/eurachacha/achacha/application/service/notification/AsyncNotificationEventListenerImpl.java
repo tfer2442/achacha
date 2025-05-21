f5970e04016730be8048ba117806ffe40201d6e8
조대성 <tfer2442@naver.com>
2025-05-21 15:57:26 +0900

    test: 비동기 알림 이벤트 로그 레벨 수정

--- backend/achacha/src/main/java/com/eurachacha/achacha/application/service/notification/AsyncNotificationEventListenerImpl.java
+++ backend/achacha/src/main/java/com/eurachacha/achacha/application/service/notification/AsyncNotificationEventListenerImpl.java
@@ -23,9 +23,9 @@ public class AsyncNotificationEventListenerImpl implements AsyncNotificationEven
 	@Override
 	public void handleNotificationEvent(NotificationEventMessage event) {
 		try {
-			log.debug("비동기 알림 이벤트 처리 시작: {}", event.getEventDto());
+			log.info("비동기 알림 이벤트 처리 시작: {}", event.getEventDto());
 			notificationEventPort.sendNotificationEvent(event.getEventDto());
-			log.debug("비동기 알림 이벤트 처리 완료");
+			log.info("비동기 알림 이벤트 처리 완료");
 		} catch (Exception e) {
 			log.error("비동기 알림 전송 실패: {}", e.getMessage(), e);
 		}
